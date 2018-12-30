14cbc6fa849be96f2fb35a1e204de0a30760ceed
mattkernow <walsh.gis@gmail.com>
2018-12-30 16:05:21 +0000

    devops

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -10,8 +10,8 @@ gcloud --quiet config set container/cluster $CLUSTER_NAME
 gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
 gcloud --quiet container clusters get-credentials $CLUSTER_NAME
 
-gcloud docker push gcr.io/${PROJECT_NAME}/${WEB_DOCKER_IMAGE_NAME}
-gcloud docker push gcr.io/${PROJECT_NAME}/${NGINX_DOCKER_IMAGE_NAME}
+docker push gcr.io/${PROJECT_NAME}/${WEB_DOCKER_IMAGE_NAME}
+docker push gcr.io/${PROJECT_NAME}/${NGINX_DOCKER_IMAGE_NAME}
 
 yes | gcloud beta container images add-tag gcr.io/${PROJECT_NAME}/${WEB_DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT gcr.io/${PROJECT_NAME}/${WEB_DOCKER_IMAGE_NAME}:latest
 yes | gcloud beta container images add-tag gcr.io/${PROJECT_NAME}/${NGINX_DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT gcr.io/${PROJECT_NAME}/${NGINX_DOCKER_IMAGE_NAME}:latest
