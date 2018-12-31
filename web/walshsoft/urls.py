ff080d50064b13ef8eab81afc61805f9439877fa
mattkernow <walsh.gis@gmail.com>
2018-12-31 15:40:01 +0000

    devops

--- web/walshsoft/urls.py
+++ web/walshsoft/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import JsonResponse
 
 from postcode_api.views import postcode
-from walshsoft.views import health
 
 urlpatterns = [
-    path(r'^$', health),
+    path('', lambda r: JsonResponse(data={'health': 'good'}), name='health'),
     path('admin/', admin.site.urls),
     path('postcode/<str:postcode>', postcode, name='postcode'),
 ]
