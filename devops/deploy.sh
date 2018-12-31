03b50383f9431f2de57f44e2692e70acc338c157
mattkernow <walsh.gis@gmail.com>
2018-12-31 16:25:56 +0000

    devops

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -11,8 +11,8 @@ gcloud --quiet config set container/cluster $CLUSTER_NAME
 gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
 gcloud --quiet container clusters get-credentials $CLUSTER_NAME
 
-docker push gcr.io/${PROJECT_NAME}/${WEB_DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT
-docker push gcr.io/${PROJECT_NAME}/${NGINX_DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT
+docker push gcr.io/${PROJECT_NAME}/${WEB_DOCKER_IMAGE_NAME}
+docker push gcr.io/${PROJECT_NAME}/${NGINX_DOCKER_IMAGE_NAME}
 
 yes | gcloud beta container images add-tag gcr.io/${PROJECT_NAME}/${WEB_DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT gcr.io/${PROJECT_NAME}/${WEB_DOCKER_IMAGE_NAME}:latest
 yes | gcloud beta container images add-tag gcr.io/${PROJECT_NAME}/${NGINX_DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT gcr.io/${PROJECT_NAME}/${NGINX_DOCKER_IMAGE_NAME}:latest
@@ -20,8 +20,8 @@ yes | gcloud beta container images add-tag gcr.io/${PROJECT_NAME}/${NGINX_DOCKER
 kubectl config view
 kubectl config current-context
 
-kubectl set image deployment/walshsoft-deployment web=${PROJECT_NAME}/${WEB_DOCKER_IMAGE_NAME} \
-                                                  nginx=gcr.io/${PROJECT_NAME}/${NGINX_DOCKER_IMAGE_NAME}
+kubectl set image deployment/walshsoft-deployment web=${PROJECT_NAME}/${WEB_DOCKER_IMAGE_NAME}:latest \
+                                                  nginx=gcr.io/${PROJECT_NAME}/${NGINX_DOCKER_IMAGE_NAME}:latest
 kubectl describe deployment walshsoft-deployment
 
 # sleep 30
