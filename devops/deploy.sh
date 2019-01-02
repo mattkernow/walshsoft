1e280789043f3342ac5473f09456fcf9e53a99f5
mattkernow <walsh.gis@gmail.com>
2019-01-02 17:26:39 +0000

    devops

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -11,6 +11,7 @@ gcloud --quiet config set container/cluster $CLUSTER_NAME
 gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
 gcloud --quiet container clusters get-credentials $CLUSTER_NAME
 
+echo "PUSHING DOCKER IMAGES TO REMOTE"
 docker push gcr.io/${PROJECT_NAME}/${WEB_DOCKER_IMAGE_NAME}
 docker push gcr.io/${PROJECT_NAME}/${NGINX_DOCKER_IMAGE_NAME}
 
@@ -21,7 +22,7 @@ kubectl config view
 kubectl config current-context
 
 kubectl set image deployment/walshsoft-deployment web=gcr.io/${PROJECT_NAME}/${WEB_DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT --record
-kubectl set image deployment/walshsoft-deployment nginx=gcr.io/${PROJECT_NAME}/${NGINX_DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT --record
+# kubectl set image deployment/walshsoft-deployment nginx=gcr.io/${PROJECT_NAME}/${NGINX_DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT --record
 
 # sleep 30
 # npm run e2e_test
