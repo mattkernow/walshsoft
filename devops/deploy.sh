d5508f99f159c08a4baa8468da2ea04668b4bc15
mattkernow <walsh.gis@gmail.com>
2019-01-13 15:26:07 +0000

    devops

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -2,19 +2,19 @@
 
 set -e
 
+if [[ ! -z ${DEPLOY_TAG} ]]; then
+    echo "PUSHING DOCKER IMAGES TO REMOTE..."
+    docker push gcr.io/${PROJECT_NAME}/${WEB_DOCKER_IMAGE_NAME}
+    docker push gcr.io/${PROJECT_NAME}/${NGINX_DOCKER_IMAGE_NAME}
 
-
-echo "PUSHING DOCKER IMAGES TO REMOTE"
-docker push gcr.io/${PROJECT_NAME}/${WEB_DOCKER_IMAGE_NAME}
-docker push gcr.io/${PROJECT_NAME}/${NGINX_DOCKER_IMAGE_NAME}
-
-yes | gcloud beta container images add-tag gcr.io/${PROJECT_NAME}/${WEB_DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT gcr.io/${PROJECT_NAME}/${WEB_DOCKER_IMAGE_NAME}:latest
-yes | gcloud beta container images add-tag gcr.io/${PROJECT_NAME}/${NGINX_DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT gcr.io/${PROJECT_NAME}/${NGINX_DOCKER_IMAGE_NAME}:latest
+    yes | gcloud beta container images add-tag gcr.io/${PROJECT_NAME}/${WEB_DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT gcr.io/${PROJECT_NAME}/${WEB_DOCKER_IMAGE_NAME}:latest
+    yes | gcloud beta container images add-tag gcr.io/${PROJECT_NAME}/${NGINX_DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT gcr.io/${PROJECT_NAME}/${NGINX_DOCKER_IMAGE_NAME}:latest
+fi
 
 kubectl config view
 kubectl config current-context
 
-echo "DEPLOYING DOCKER IMAGES"
+echo "PERFORM ROLLING UPDATE..."
 
 kubectl set image deployment/walshsoft-deployment web=gcr.io/${PROJECT_NAME}/${WEB_DOCKER_IMAGE_NAME}:$TAG --record
 kubectl set image deployment/walshsoft-deployment nginx=gcr.io/${PROJECT_NAME}/${NGINX_DOCKER_IMAGE_NAME}:$TAG --record
