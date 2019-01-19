5ba147054a93b282f895aa52f8974a15c9a527b7
mattkernow <walsh.gis@gmail.com>
2019-01-19 12:31:54 +0000

    move integration tests to deploy stage

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -17,3 +17,7 @@ echo "PERFORM ROLLING UPDATE..."
 
 kubectl set image deployment/walshsoft-deployment web=gcr.io/${PROJECT_NAME}/${WEB_DOCKER_IMAGE_NAME}:$TAG --record
 kubectl set image deployment/walshsoft-deployment nginx=gcr.io/${PROJECT_NAME}/${NGINX_DOCKER_IMAGE_NAME}:$TAG --record
+
+echo "RUNNING INTEGRATION TESTS POST DEPLOYMENT"
+sleep 60
+docker run --rm --env POSTCODE_HOST=35.228.129.21 --env POSTCODE_PORT=80 gcr.io/${PROJECT_NAME}/${WEB_DOCKER_IMAGE_NAME}:$TAG /bin/sh -c "pytest --disable-pytest-warnings /code/postcode_api/tests/tests_integration"
